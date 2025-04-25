75cbd116adc5179d2f9428247b3c7dfe0ba2e814
valepw <valevale.pw@gmail.com>
2025-04-24 23:00:39 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+// Funcionalidad para mostrar/ocultar secciones
 document.getElementById('mezclas-btn').addEventListener('click', function() {
     document.getElementById('mezclas').classList.toggle('hidden');
-    document.getElementById('menu').classList.add('hidden'); // Ocultar menú
 });
 
 document.getElementById('menu-btn').addEventListener('click', function() {
     document.getElementById('menu').classList.toggle('hidden');
-    document.getElementById('mezclas').classList.add('hidden'); // Ocultar mezclas
 });
