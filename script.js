8c8a16c50eec3cc0d0ae994bebe772eebd0b1ba9
valepw <valevale.pw@gmail.com>
2025-04-24 22:42:36 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("pedido").addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("Tu pedido está vacío. ¡Agrega un café para comenzar!");
-  });
-});
+function mostrarSeccion(id) {
+  const secciones = document.querySelectorAll('.seccion');
+  secciones.forEach(seccion => seccion.classList.remove('activa'));
+  document.getElementById(id).classList.add('activa');
+}
