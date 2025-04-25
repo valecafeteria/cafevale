ef39d79370f1747f50f036ff1c00e45969334fc4
valepw <valevale.pw@gmail.com>
2025-04-24 22:29:08 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("pedido").addEventListener("click", (e) => {
     e.preventDefault();
-    alert("Tu pedido está vacío. ¡Agrega algo delicioso!");
-  });
-
-  document.getElementById("buscar").addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("Función de búsqueda en desarrollo.");
-  });
-
-  document.getElementById("cuenta").addEventListener("click", (e) => {
-    e.preventDefault();
-    alert("Inicia sesión próximamente.");
+    alert("Tu pedido está vacío. ¡Agrega un café para comenzar!");
   });
 });
