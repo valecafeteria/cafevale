d9275ad47dc7333b141a8f2d439721c6d26797c1
valepw <valevale.pw@gmail.com>
2025-05-31 22:29:47 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 let carrito = [];
 
-function agregarAlCarrito(producto, precio) {
-  carrito.push({ producto, precio });
-  alert(`Agregado: ${producto} - $${precio}`);
+function agregarAlCarrito(nombreProductoBase, precioBase, idProducto) {
+  const tamaño = document.querySelector(`#${idProducto} select[name="tamaño"]`).value;
+  const leche = document.querySelector(`#${idProducto} select[name="leche"]`).value;
+  const complemento = document.querySelector(`#${idProducto} select[name="complemento"]`).value;
+
+  let precioFinal = precioBase;
+
+  // Ajuste de precio por tamaño
+  if (tamaño === "Mediano") precioFinal += 5;
+  else if (tamaño === "Grande") precioFinal += 10;
+
+  // Ajuste de precio por leche
+  if (leche === "Deslactosada" || leche === "Vegetal") precioFinal += 5;
+
+  // Ajuste de precio por complemento
+  if (complemento !== "Ninguno") precioFinal += 10;
+
+  const productoCompleto = `${nombreProductoBase} (${tamaño}, Leche: ${leche}, Comp: ${complemento})`;
+
+  carrito.push({ producto: productoCompleto, precio: precioFinal });
+
+  alert(`Agregado: ${productoCompleto} - $${precioFinal}`);
 }
 
 function abrirCarrito() {
